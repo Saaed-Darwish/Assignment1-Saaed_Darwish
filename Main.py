19a27790c0ab7c6225b9573f5d6d3e87b5ab3468
Saaed Darwish <saaeddarwish1@gmail.com>
2022-10-03 22:24:41 -0600

    Commentting Code Blocks

---
+++ Main.py
@@ -0,0 +1,21 @@
+# calculator.py
+# Saaed Darwish, ENSF 300 F22
+#
+# A terminal-based calculator application for determining the result of a mathematical expression containing three values and two operators.
+#
+
+# function of addition
+
+# function of subtraction
+
+# function of multiplication
+
+# function of division
+
+# user prompts
+
+# precedence determination
+
+# calculations with functions
+
+# answers & output
