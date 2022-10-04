7bffaa033d86a5e747470abb8ba89beec33e90b5
Saaed Darwish <saaeddarwish1@gmail.com>
2022-10-03 22:30:05 -0600

    Subtraction Function Implemented

--- Main.py
+++ Main.py
@@ -7,6 +7,19 @@
 # function of addition
 
 # function of subtraction
+def subtraction(subtract_value1, subtract_value2):
+    '''
+    Addition of two values together
+
+    Parameters:
+    subtract_value1 -- the first value to be subtracted by the second value
+    subtract_value2 -- the second value to subtract from the first value
+
+    Returns:
+    subtracted_value -- the value of the subtraction of the two values
+    '''
+    subtracted_value = subtract_value1 - subtract_value2
+    return subtracted_value
 
 # function of multiplication
 
