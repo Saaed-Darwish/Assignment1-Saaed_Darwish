6a56a99bfd74eaf55836fa2e18f410dd22b64649
Saaed Darwish <saaeddarwish1@gmail.com>
2022-10-03 22:55:50 -0600

    Merge branch 'Addition'

--- Main.py
+++ Main.py
@@ -5,6 +5,19 @@
 #
 
 # function of addition
+def addition(add_value1, add_value2):
+    '''
+    Addition of two values together
+
+    Parameters:
+    add_value1 -- the first value to be added with the second value
+    add_value2 -- the second value to be added with the first value
+
+    Returns:
+    added_value -- the value of the addition of the two values
+    '''
+    added_value = add_value1 + add_value2
+    return added_value
 
 # function of subtraction
 
