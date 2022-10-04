c75c8c55641c5a45fb52796eb15058df1764945e
Saaed Darwish <saaeddarwish1@gmail.com>
2022-10-03 22:33:12 -0600

    Commentting Code Blocks

---
+++ Main.py
@@ -0,0 +1,34 @@
+# calculator.py
+# Saaed Darwish, ENSF 300 F22
+#
+# A terminal-based calculator application for determining the result of a mathematical expression containing three values and two operators.
+#
+
+# function of addition
+
+# function of subtraction
+
+# function of multiplication
+def multiplication(multiply_value1, multiply_value2):
+    '''
+    Addition of two values together
+
+    Parameters:
+    multiply_value1 -- the first value to be multiplied with the second value
+    multiply_value2 -- the second value to be multipled with the first value
+
+    Returns:
+    multiplied_value -- the value of the multiplication of the two values
+    '''
+    multiplied_value = multiply_value1 + multiply_value2
+    return multiplied_value
+
+# function of division
+
+# user prompts
+
+# precedence determination
+
+# calculations with functions
+
+# answers & output
